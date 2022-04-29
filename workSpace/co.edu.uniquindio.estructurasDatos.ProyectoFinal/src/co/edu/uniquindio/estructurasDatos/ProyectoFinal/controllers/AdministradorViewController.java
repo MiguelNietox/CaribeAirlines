0a418452b90d579a545853e5d1f40ca80fd4de3a
MiguelNietox <miguelnieto.08@gmail.com>
2022-04-29 09:48:07 -0500

    Login implementado

---
+++ workSpace/co.edu.uniquindio.estructurasDatos.ProyectoFinal/src/co/edu/uniquindio/estructurasDatos/ProyectoFinal/controllers/AdministradorViewController.java
@@ -0,0 +1,12 @@
+package co.edu.uniquindio.estructurasDatos.ProyectoFinal.controllers;
+
+import co.edu.uniquindio.estructurasDatos.ProyectoFinal.aplication.Aplicacion;
+
+public class AdministradorViewController {
+
+	public void setAplicacion(Aplicacion aplicacion) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ workSpace/co.edu.uniquindio.estructurasDatos.ProyectoFinal/src/co/edu/uniquindio/estructurasDatos/ProyectoFinal/controllers/ClienteViewController.java
@@ -0,0 +1,12 @@
+package co.edu.uniquindio.estructurasDatos.ProyectoFinal.controllers;
+
+import co.edu.uniquindio.estructurasDatos.ProyectoFinal.aplication.Aplicacion;
+
+public class ClienteViewController {
+
+	public void setAplicacion(Aplicacion aplicacion) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
